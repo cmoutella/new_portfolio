b9c423ac2ab54ebf57c1f5a18ac23fee5a3e76db
Carolina Moutella <emaildacarolmoutella@gmail.com>
2020-03-25 15:13:54 -0300

    Configure vue-router

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+// Views
+import Home from './views/Home/index.vue';
+import Portfolio from './views/Portfolio/index.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio,
+    },
+  ],
+});
