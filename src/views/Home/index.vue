c4ccb2252a83a04e3758dde9412d40629ca1acfc
Carolina Moutella <emaildacarolmoutella@gmail.com>
2020-03-27 09:59:49 -0300

    Top banner

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="devm-home">
-    <h1>Home</h1>
+    <div class="devm-top">
+      <div class="container">
+        <p class="devm-name">Carolina Moutella<span>_</span></p>
+        <ul class="devm-language">
+          <li class="devm-language-option devm-language-option--active">
+            <input type="radio" v-model="language" value="1" label="PT" id="lang_portuguese" />
+            <label for="lang_portuguese">PT</label>
+          </li>
+          <li class="devm-language-option">
+            <input type="radio" v-model="language" value="1" label="EN" id="lang_english" />
+            <label for="lang_english">EN</label>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="devm-home-content"></div>
   </div>
 </template>
 
