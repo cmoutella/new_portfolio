301e642d7a8bfcea78b1e61fa104103ddb43a177
Carolina Moutella <emaildacarolmoutella@gmail.com>
2020-03-27 17:40:42 -0300

    Style top

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,6 +3,7 @@
     <div class="devm-top">
       <div class="container">
         <p class="devm-name">Carolina Moutella<span>_</span></p>
+        <span class="devm-toon"></span>
         <ul class="devm-language">
           <li class="devm-language-option devm-language-option--active">
             <input type="radio" v-model="language" value="1" label="PT" id="lang_portuguese" />
